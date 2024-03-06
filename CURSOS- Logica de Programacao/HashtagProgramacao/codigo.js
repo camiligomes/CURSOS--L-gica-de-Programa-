ec7932d043cc1cc5039ab33fc4e78cdcb801d758
Camili <124635963+camiligomes@users.noreply.github.com>
2024-03-05 21:48:07 -0300

    Update codigo.js

--- CURSOS- Logica de Programacao/HashtagProgramacao/codigo.js
+++ CURSOS- Logica de Programacao/HashtagProgramacao/codigo.js
@@ -9,14 +9,14 @@ while (notasLancadas < 12) {
     const nota = Number(prompt(`${notasLancadas + 1}a nota do ${aluno}: `));
     console.log(`A nota inserida foi ${nota}`);
     pontosSomados = pontosSomados + nota;
-  notasLancadas = notasLancadas+ 1;
-  mediaPontos= pontosSomados / notasLancadas;
-  
-console.log(`A média final do aluno ${aluno} é ${mediaPontos.toFixed(2)}`);
+    notasLancadas = notasLancadas + 1;
+    mediaPontos = pontosSomados / notasLancadas;
+
+    console.log(`Media ate ${notasLancadas} a nota: ${mediaPontos}`)
 }
 
-
-
+console.log(`A media final do aluno ${aluno} foi ${mediaPontos}`);
+ 
 if (mediaPontos >= 6) {
     console.log(`O aluno ${aluno} foi aprovado`);
 } else {
